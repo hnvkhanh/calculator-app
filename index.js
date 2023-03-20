294731f64ff2b8625fef4ac873f1caf206761d81
hnvkhanh <hnvk101@gmail.com>
2023-03-20 13:49:09 +0700

    add calc function

---
+++ index.js
@@ -0,0 +1,99 @@
+var keys = document.querySelectorAll("#key-number");
+var operators = document.querySelectorAll("#operator");
+let dotKey = document.getElementById("key-dot");
+let deleteKey = document.getElementById("key-delete");
+let equalSign = document.getElementById("equal-sign");
+let resetKey = document.getElementById("reset-calc");
+let textOnScreen = document.getElementById("text-on-screen");
+let dotKeyClicked = false;
+let operatorClicked = false;
+let clickedKeys = [];
+let currentOperator;
+let clickedString = "0";
+let inputNum;
+
+function processMath(operator, a, b) {
+    switch (operator) {
+        case '+':
+          return a + b;          
+        case '-':
+            return a - b;
+        case '/':
+          return a / b;
+        default:
+          return a * b;
+    }
+      
+}
+
+
+keys.forEach((key) =>{
+    key.addEventListener("click", () => {
+        clickedKeys.push(key.innerHTML);
+        clickedString = clickedKeys.reduce((a, b) => {return a + b;});      
+        textOnScreen.innerText = clickedString.replace(".", ",");  
+    })
+})
+
+dotKey.addEventListener("click", () => {
+    if (clickedKeys.length > 0 && !dotKeyClicked){
+        clickedKeys.push(".");
+        dotKeyClicked = true;
+        clickedString = clickedKeys.reduce((a, b) => {return a + b;});
+        textOnScreen.innerText = clickedString.replace(".", ",");
+    }    
+})
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", () => {
+        dotKeyClicked = false;
+        if (!operatorClicked){
+            inputNum = parseFloat(clickedString);
+            currentOperator = operator.innerText;
+            clickedString = "";
+            clickedKeys = [];
+            operatorClicked = true;            
+        }
+        else{
+            inputNum = processMath(currentOperator, inputNum, parseFloat(clickedString))
+            clickedString = "";
+            clickedKeys = [];
+            textOnScreen.innerText = inputNum;
+            currentOperator = operator.innerText;
+        }        
+    });    
+});
+
+equalSign.addEventListener("click", () => {
+    dotKeyClicked = false;
+    if (operatorClicked){
+        inputNum = processMath(currentOperator, inputNum, parseFloat(clickedString));
+        clickedString = inputNum;
+        clickedKeys = [];
+        textOnScreen.innerText = inputNum;
+        operatorClicked = false;
+    }    
+});
+
+resetKey.addEventListener("click", () => {
+    dotKeyClicked = false;
+    clickedString = "0";
+    clickedKeys = [];
+    currentOperator = undefined;
+    operatorClicked = false;
+    textOnScreen.innerText = 0;
+});
+
+deleteKey.addEventListener("click", () => {
+    let deletedKey = clickedKeys.pop();
+    if (clickedKeys.length > 0){
+        clickedString = clickedKeys.reduce((a, b) => {return a + b;});
+    }
+    else{
+        clickedString = "0";
+    }
+    if (deleteKey == "."){
+        dotKeyClicked = false;
+    }
+    textOnScreen.innerText = clickedString.replace(".", ",");
+})
